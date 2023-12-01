5a67b4e09a7cd9842903bab2420a1cd2c541e1ad
TulipeSoyeuse <romain.dupeux24@gmail.com>
2023-12-01 14:54:58 +0100

    moult choses

--- pushswapemulator/__main__.py
+++ pushswapemulator/__main__.py
@@ -1,5 +1,7 @@
 import argparse
+import random
 from collections import deque
+from subprocess import PIPE, run
 from sys import argv, stdin
 
 from colorama import Fore
@@ -14,14 +16,65 @@ base on a list of command.
 Can be lauch on interactive mode for more fun."""
 )
 
-parser.add_argument("--it", action="store_true")
-parser.add_argument("--check", nargs="*")
-parser.add_argument("stack", nargs="*", type=int)
-parser.add_argument("--pipe", action="store_true")
+parser.add_argument(
+    "--it",
+    action="store_true",
+    help="iteractive mode, create stack and let you experiment,\n\
+    all pushswap cmd are available plus 'reset' who reset the stack at his original value",
+)
+parser.add_argument(
+    "--check",
+    nargs="*",
+    help="pass a list of operations to the program, quit at the end of the list",
+)
+parser.add_argument(
+    "stack",
+    nargs="*",
+    type=int,
+    help="""list of integers to be imputed as stack,
+                    if only one number is given, it will be used as the borne for a range of number""",
+)
+parser.add_argument(
+    "--pipe",
+    action="store_true",
+    help="pass your pushswap result in the stdin of this program",
+)
+parser.add_argument(
+    "--exec",
+    nargs=1,
+    type=str,
+    help="use this flag to input your C executable directly to the program",
+)
+parser.add_argument("--test", action="store_true")
 
 args = parser.parse_args()
-swapper = Swapper(args.stack)
+
+if len(args.stack) > 1:
+    swapper = Swapper(args.stack)
+else:
+    stack = list(range(0, args.stack[0]))
+    random.shuffle(stack)
+    swapper = Swapper(stack)
+
+if args.test :
+    cmd = [str(i) for i in swapper.stack_a]
+    cmd.insert(0,"")
+    print(f"{args.exec[0]} ", " ".join(cmd))
+    res = run(cmd, executable=args.exec[0], capture_output=True, text=True, timeout=40)
+    print(res.stdout)
+    exit(0)
+
 swapper.display_env("")
+
+if args.exec:
+    cmd = [str(i) for i in swapper.stack_a]
+    cmd.insert(0,"")
+    res = run(cmd, executable=args.exec[0], capture_output=True, text=True)
+    cmds = res.stdout.split("\n")
+    for cmd_out in cmds:
+        swapper.parse_input(cmd_out)
+        resolution(swapper, cmd_out)
+
 if args.it and (args.check or args.pipe):
     print("it and check/pipe command are imcompatible")
     exit(1)
@@ -38,7 +91,7 @@ if args.check:
         resolution(swapper, cmd)
 
 if args.pipe:
-    cmds = stdin.read().split('\n')[:-1]
+    cmds = stdin.read().split("\n")[:-1]
     for cmd in cmds:
         swapper.parse_input(cmd)
-        resolution(swapper)
+        resolution(swapper, cmd)
